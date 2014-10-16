b78acfcf192f08488cc0c7c644df29ccd66d2280
dkanafeev <domir332@live.ru>
2014-10-16 22:55:41 +0400

    Added hw02

---
+++ KanafeevD/hw02/pnum.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Script prime number  V.0.1
+
+function pnum()
+{
+	echo "Start pnum..."
+	echo "Param $1"
+
+	red='\e[0;31m'
+	green='\e[0;32m'
+	noColor='\e[0m' 
+
+	i=2
+ 
+	if [ "$1" -eq 2 ] ; 
+	then 
+		echo -e "${green}$1 is primary number!${noColor}"
+		return 0
+	fi
+ 
+	while  [ $(($i*$i))  -le "$1" ] ; do
+		if  [ "$(($1%$i))" -eq 0 ] ; then
+			echo -e "${red}$1 is not primary number!${noColor}"
+			return 0
+		fi
+		i=$(($i+1))
+	done
+	echo -e "${green}$1 is primary number!${noColor}"
+
+	echo "End pnum"
+
+	return 0
+}
