5ead62f52447a72f2e89dadb15cbfd23a227b735
mexanik72 <andrew6363@mail.ru>
2014-12-17 12:32:41 -0800

    change otkritka

--- GalochkinA/perl/5/5otkritka.pl
+++ GalochkinA/perl/5/5otkritka.pl
@@ -2,7 +2,9 @@
 use 5.18.2;
 use strict;
 use warnings;
+use utf8; 
 
+binmode(STDOUT,':utf8');
 my $filename = "datafor.txt";
 my $filename2 = "otkritka.html";
 my $filenameoutput = "otkritkaForYou.html";
@@ -42,62 +44,62 @@ open($fh, '>:encoding(UTF-8)', $filenameoutput)
 or die "Could not open file '$filename' $!";
 print $fh join("\n",@bd);
 if($name[0] eq '0'){
-print $fh "Uvajaemii ";
+print $fh "Уважаемый ";
 }
 else{
-print $fh "Uvajaemaya ";
+print $fh "Уважаемая ";
 }
 print $fh "$name[1] ";
-print $fh "$name[2], <br />";
+print $fh "$name[2],<br />";
 ;
 if($name[3] eq 0){
 print $fh "...";
 }
 if($name[3] eq 1){
-print $fh "...";
+print $fh "Поздравляю";
 }
 if($name[3] eq 2){
-print $fh "S novim godom.";
+print $fh "С новым годом.";
 }
 if($name[3] eq 3){
-print $fh "S novim godom!:)";
+print $fh "С новым годом!:)";
 }
 if($name[3] eq 4){
-print $fh "S novim godom!<br /> s4ast'ya";
+print $fh "С новым годом!<br /> счастья";
 }
 if($name[3] eq 5){
-print $fh "S novim godom!<br /> Uda4i i prozvitaniya v novom godu";
+print $fh "С новым годом!<br /> Удачи и процвитания в новом году";
 }
 if($name[3] eq 6){
-print $fh "S novim godom!<br /> S4ast'ya, zdorovia i uda4i v novom godu";
+print $fh "С новым Годом!<br /> Счастья, здоровья и удачи в новом году!";
 }
 if($name[3] eq 7){
-print $fh "Jelau vstretit Novii god<br />
-Veseliem shumnim i s razmahom.";
+print $fh "Желаем встретить Новый год<br />
+Весельем шумным и с размахом.<br />";
 }
 if($name[3] eq 8){
-print $fh "Jelau vstretit Novii god<br />
-Veseliem shumnim i s razmahom.<br />
-Pust' prazdnik radost' prineset,<br />
-I eto budet dobrim znakom.";
+print $fh "Желаем встретить Новый год<br />
+Весельем шумным и с размахом.<br />
+Пусть праздник радость принесёт,<br />
+И это будет добрым знаком.";
 }
 if($name[3] eq 9){
-print $fh "Jelau vstretit Novii god<br />
-Veseliem shumnim i s razmahom.<br />
-Pust' prazdnik radost' prineset,<br />
-I eto budet dobrim znakom.<br />
-Jelaem yarkih perspectiv,<br />
-Udachi, smelih na4inanii.";
+print $fh "Желаем встретить Новый год<br />
+Весельем шумным и с размахом.<br />
+Пусть праздник радость принесёт,<br />
+И это будет добрым знаком.<br />
+Желаем ярких перспектив,<br />
+Удачи, смелых начинаний.<br />";
 }
 if($name[3] eq 10){
-print $fh "Jelau vstretit Novii god<br />
-Veseliem shumnim i s razmahom.<br />
-Pust' prazdnik radost' prineset,<br />
-I eto budet dobrim znakom.<br />
-Jelaem yarkih perspectiv,<br />
-Udachi, smelih na4inanii.<br />
-Pust' v zhizni budet pozitiv<br />
-I mnogo sbivshihsya zhelanii!<br />";
+print $fh "Желаем встретить Новый год<br />
+Весельем шумным и с размахом.<br />
+Пусть праздник радость принесёт,<br />
+И это будет добрым знаком.<br />
+Желаем ярких перспектив,<br />
+Удачи, смелых начинаний.<br />
+Пусть в жизни будет позитив<br />
+И много сбывшихся желаний!<br />";
 }
 close $fh;
 
