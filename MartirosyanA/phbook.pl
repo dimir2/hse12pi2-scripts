27408c7c8ee563e9ca0b17f60399cfef58ab6281
Alisa Martirosyan <numiareced@gmail.com>
2014-12-18 19:18:28 +0300

    Signed-off-by: Alisa Martirosyan <numiareced@gmail.com>

---
+++ MartirosyanA/phbook.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+print "input 1 to find 2 to add 3 to delete \n"; $temp1=<STDIN>;
+if($temp1==1) {
+$temp2=<STDIN>;
+chomp $temp2;
+open (PHONE, "phone.txt") || die "error :$! \n";
+while (<PHONE>) {
+chomp; 
+($name, $phone)=split /::/, $_; 
+if ($name eq $temp2 or $phone eq $temp2) {
+print "surname: $name Phone: $phone \n";
+}
+}
+close PHONE; 
+};
+if ($temp1==2) {
+print "input the name and number \n";
+$add=<STDIN>; 
+chomp $add; 
+open (PHONE, '>>', "phone.txt") || die "error :$! \n";
+print PHONE $add, "\n";
+}
+if ($temp1==3) {
+print "input the name u want to delete:";
+$delete=<STDIN>;
+chomp $delete;
+open(PHONE, "phone.txt");
+@my_hash = grep {!/$delete/} <PHONE>;
+close PHONE;
+open PHONE, ">phone.txt";
+foreach (@my_hash) {
+print PHONE;
+}
+}
+
