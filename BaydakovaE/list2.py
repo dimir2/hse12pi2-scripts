ef7351a3a4dbf7a0ef7c6dcf9daf51b11093b906
BaydakovaE <elisabeth2307@mail.ru>
2015-02-17 22:09:01 +0300

    Create list2.py

---
+++ BaydakovaE/list2.py
@@ -0,0 +1,41 @@
+# D. Given 
+def remove_adjacent(nums):
+    s=[]
+    i=1
+    while i < len(nums):
+        if nums[i]==nums[i-1]:
+            nums.pop(i)
+            i=i-1
+        i=i+1
+    return nums
+
+# E. Given two lists sorted
+def linear_merge(list1, list2):
+    list1=sorted(list1+list2)
+    return list1
+
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print ('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+def main():
+  print ('remove_adjacent')
+  test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+  test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+  test(remove_adjacent([]), [])
+
+  print
+  print ('linear_merge')
+  test(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+       ['aa', 'bb', 'cc', 'xx', 'zz'])
+  test(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+       ['aa', 'aa', 'aa', 'bb', 'bb'])
+
+
+if __name__ == '__main__':
+  main()
